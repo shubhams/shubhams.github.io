cc9f388d13fc99f7fc60a0cf675aa9932c2f5fc9
shubhams <shubhamsingh19294@gmail.com>
2025-11-23 21:01:40 +0000

    Deploying to gh-pages from @ shubhams/shubhams.github.io@f8c920d6977435f768c61ffb7cccba3205ef42fb 🚀

---
+++ assets/js/cronitor-analytics-setup.js
@@ -0,0 +1,6 @@
+window.cronitor =
+  window.cronitor ||
+  function () {
+    (window.cronitor.q = window.cronitor.q || []).push(arguments);
+  };
+cronitor("config", { clientKey: "fe42af14ddcfb7755b59b8592ca37e35" });
