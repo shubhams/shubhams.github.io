a6e3f1a9fa0d1bd07ad37e88d22e9342f1dbd614
shubhams <shubhamsingh19294@gmail.com>
2025-12-01 17:56:19 +0000

    Deploying to gh-pages from @ shubhams/shubhams.github.io@5da259bc0c23ee7cb837b148b6da492eaeb42ecd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -276,6 +276,17 @@ ninja.data = [{
             window.location.href = "/sample-posts/external-services/2022/12/10/giscus-comments.html";
           
         },
+      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
+        
+          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+          
+        },
       },{id: "post-a-post-with-redirect",
         
           title: "a post with redirect",
@@ -380,18 +391,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-our-paper-titled-helping-users-automatically-find-and-manage-sensitive-expendable-files-in-cloud-storage-has-been-accepted-at-the-30th-usenix-security-symposium-the-camera-ready-version-of-the-paper-is-available-at-the-link",
+            },},{id: "news-our-paper-titled-helping-users-automatically-find-and-manage-sensitive-expendable-files-in-cloud-storage-has-been-accepted-at-the-30th-usenix-security-symposium-the-camera-ready-version-of-the-paper-is-available-at-the-link",
           title: 'Our paper titled “Helping Users Automatically Find and Manage Sensitive, Expendable Files in...',
           description: "",
           section: "News",},{id: "news-i-am-one-of-the-graduate-teaching-assistants-for-the-spring-2021-offering-of-cs-361-computer-systems-programming",
